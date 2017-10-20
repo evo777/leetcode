7c9dd5d47bf7b425d1dff317a6a54fa7a94c2119
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-10-19 17:16:35 -0700

    Initial commit

---
+++ 1. Two Sums/solution.js
@@ -0,0 +1,20 @@
+/***
+
+Write a function
+
+***/
+
+const twoSum = (nums, target) => {
+    let map = [];
+
+    for(let i = 0; i < nums.length; i++) {
+        let found = target - nums[i];
+        if(map[nums[i]] !== undefined) {
+            return [map[nums[i]], i]
+        } else {
+            map[found] = i;
+        }
+    }
+
+    return [];
+};

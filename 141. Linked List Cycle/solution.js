2c96342ac728130f2aa11b87ab7170068a30d4b8
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-10-22 22:06:27 -0700

    Completed

---
+++ 141. Linked List Cycle/solution.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(head === null || head.next === null){
+        return false;
+    }
+
+    var slow = head.next;
+    var fast = head.next.next;
+    if(fast === null){
+        return false;
+    }
+
+    while(slow != fast){
+        if(fast.next === null){
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow === null || fast === null){
+            return false;
+        }
+    }
+    return true;
+};

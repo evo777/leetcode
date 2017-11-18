c010906d18eaadb481b6e2e30851d233960db9d5
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-11-18 13:50:04 -0800

    Completed

---
+++ 667. Map Sum Pair/solution.js
@@ -0,0 +1,29 @@
+var MapSum = function() {
+  this.obj = {};
+};
+
+/**
+ * @param {string} key
+ * @param {number} val
+ * @return {void}
+ */
+MapSum.prototype.insert = function(key, val) {
+  if (this.obj[key]) {
+      this.obj[key] = val;
+  }
+  this.obj[key] = val;
+};
+
+/**
+ * @param {string} prefix
+ * @return {number}
+ */
+MapSum.prototype.sum = function(prefix) {
+  let count = 0;
+  for(key in this.obj) {
+      if (key.startsWith(prefix)) {
+          count = count + this.obj[key];
+      }
+  }
+  return count;
+};

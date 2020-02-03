f6d322a984cf7eb73abd86cecda9fe8d79315fd5
Emmanuel Vo <emmanuelvo777@gmail.com>
2020-02-02 20:21:40 -0800

    Solve using brute force in PHP

---
+++ 1. Two Sums/PHP/solution.php
@@ -0,0 +1,20 @@
+<?php
+class Solution
+{
+
+/**
+ * @param Integer[] $nums
+ * @param Integer $target
+ * @return Integer[]
+ */
+    public function twoSum($nums, $target)
+    {
+        for ($i = 0; $i < count($nums); $i++) {
+            for ($j = $i + 1; $j < count($nums); $j++) {
+                if ($nums[$i] + $nums[$j] == $target) {
+                    return [$i, $j];
+                }
+            }
+        }
+    }
+}

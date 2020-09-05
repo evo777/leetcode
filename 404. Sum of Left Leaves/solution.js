ce97c0d7d4d49209c132f28b7c3fadad4ec32362
Emmanuel Vo <emmanuelvo777@gmail.com>
2020-09-05 16:13:56 -0700

    Add explaination

--- 404. Sum of Left Leaves/solution.js
+++ 404. Sum of Left Leaves/solution.js
@@ -2,12 +2,15 @@ var sumOfLeftLeaves = function (root) {
   let sum = 0;
 
   function dfsTraversal(node) {
+    // If we reach the bottom, break out of recursion
     if (!node) return;
 
+    // If we are on the left and the left node has no right or left children, add to sum
     if (node.left && !node.left.left && !node.left.right) {
       sum += node.left.val;
     }
 
+    // Recurse
     dfsTraversal(node.left);
     dfsTraversal(node.right);
   }
